f4ac3157c3a4013cde4fcd3653df56ce3a4dc981
Osimi Godsgift <114400012+Coding-Base@users.noreply.github.com>
2025-07-04 10:36:28 +0100

    Add files via upload

--- exams/storage_backends.py
+++ exams/storage_backends.py
@@ -1,40 +1,39 @@
-# storage_backends.py
 # exams/storage_backends.py
-from django.conf import settings
-from google.cloud import storage
+import os
+import json
 from django.core.files.storage import Storage
 from django.utils.deconstruct import deconstructible
+from google.cloud import storage
 from google.oauth2 import service_account
 from google.api_core.exceptions import NotFound
-import os
+
 
 @deconstructible
 class GoogleCloudMediaStorage(Storage):
     def __init__(self):
-        creds_path = settings.GOOGLE_APPLICATION_CREDENTIALS
-        credentials = service_account.Credentials.from_service_account_file(creds_path)
-        self.client = storage.Client(credentials=credentials)
-        self.bucket = self.client.bucket(settings.GS_BUCKET_NAME)
-    
+        credentials_info = json.loads(os.environ["GCS_CREDENTIALS_JSON"])
+        credentials = service_account.Credentials.from_service_account_info(credentials_info)
+        self.client = storage.Client(
+            credentials=credentials,
+            project=os.environ.get("GS_PROJECT_ID")
+        )
+        self.bucket = self.client.bucket(os.environ.get("GS_BUCKET_NAME"))
+
     def _save(self, name, content):
         blob = self.bucket.blob(name)
-        
-        # Disable ACLs and use IAM policies
         blob.upload_from_file(
             content,
             content_type=content.content_type,
-            # Prevent ACL-related operations
             predefined_acl=None,
             if_generation_match=None
         )
         return name
-    
+
     def exists(self, name):
         try:
             return self.bucket.blob(name).exists()
         except NotFound:
             return False
-    
+
     def url(self, name):
-        # Direct public URL format
         return f"https://storage.googleapis.com/{self.bucket.name}/{name}"
