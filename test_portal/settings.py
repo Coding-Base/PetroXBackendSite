91f5dfb050766ebe4477fa31adc10231811c2cdf
Osimi Godsgift <114400012+Coding-Base@users.noreply.github.com>
2025-08-01 11:35:15 +0100

    Update settings.py

--- test_portal/settings.py
+++ test_portal/settings.py
@@ -193,7 +193,7 @@ if not DEBUG:
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],  # ADD THIS LINE
+        'DIRS': [os.path.join(BASE_DIR, 'exams', 'templates')],  # ADD THIS LINE
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
