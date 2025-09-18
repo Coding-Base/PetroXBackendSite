4c5804cbe711a48202488ef44a978c220b1e2975
Osimi Godsgift <114400012+Coding-Base@users.noreply.github.com>
2025-09-18 16:03:24 +0100

    Update urls.py

--- test_portal/urls.py
+++ test_portal/urls.py
@@ -1,4 +1,3 @@
-# test_portal/urls.py
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import (
@@ -6,7 +5,8 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
     TokenBlacklistView
 )
-from exams.views.auth import RegisterUserAPIView,GoogleAuthView
+from exams.views.auth import RegisterUserAPIView, GoogleAuthView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
@@ -14,6 +14,7 @@ urlpatterns = [
     path('api/token/blacklist/', TokenBlacklistView.as_view(), name='token_blacklist'),
     
     path('users/', RegisterUserAPIView.as_view(), name='register-user-root'),
-    path('api/auth/google/', GoogleAuthView.as_view(), name='google-auth'),  # Add this line
+    path('api/auth/google/', GoogleAuthView.as_view(), name='google-auth'),
     path('api/', include('exams.urls')),
+    path('api/', include('updates.urls')),  # Add this line to include updates URLs
 ]
