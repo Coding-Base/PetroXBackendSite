70445202f51c2d429965169c36578b47322b1347
Osimi Godsgift <114400012+Coding-Base@users.noreply.github.com>
2025-11-03 15:34:23 +0100

    Update urls.py

--- test_portal/urls.py
+++ test_portal/urls.py
@@ -6,15 +6,16 @@ from rest_framework_simplejwt.views import (
     TokenBlacklistView
 )
 from exams.views.auth import RegisterUserAPIView, GoogleAuthView
-
+from exams.views import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/blacklist/', TokenBlacklistView.as_view(), name='token_blacklist'),
-    
+    path('admin/trigger-send/', views.trigger_render_job, name='trigger_render_job'),
     path('users/', RegisterUserAPIView.as_view(), name='register-user-root'),
     path('api/auth/google/', GoogleAuthView.as_view(), name='google-auth'),
     path('api/', include('exams.urls')),
     path('api/', include('updates.urls')),  # Add this line to include updates URLs
 ]
+
